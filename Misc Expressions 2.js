33dc3844644fbaa7865b4308317bf1d85ebf1881
Kevin <Kevin.Thompson@ArizonaCoyotes.com>
2021-08-19 13:10:42 -0700

    Initial commit.

---
+++ Misc Expressions 2.js
@@ -0,0 +1,62 @@
+// Previous Expressions used to access data from .csv spreadsheets generated from Sportsipy
+footage("Clayton Keller.csv").dataValue([43,thisComp.layer("Clayton Keller.csv")("Effects")("Spreadsheet Row")("Slider")-1])
+
+yearrow = "year " + Math.floor(thisComp.layer("Clayton Keller.csv")("Effects")("Spreadsheet Row")("Slider"));
+thisComp.layer("Clayton Keller.csv")("ADBE Data Group")("Outline")("year")(yearrow)
+
+shots_on_goalrow = "shots_on_goal " + (Math.floor(thisComp.layer("Clayton Keller.csv")("Effects")("Spreadsheet Row")("Slider")) - 1);
+thisComp.layer("Clayton Keller.csv")("ADBE Data Group")("Outline")("shots_on_goal")(shots_on_goalrow)
+
+shots_on_goalrow = "shots_on_goal " + Math.floor(thisComp.layer("Clayton Keller.csv")("Effects")("Spreadsheet Row")("Slider"));
+thisComp.layer("Clayton Keller.csv")("ADBE Data Group")("Outline")("shots_on_goal")(shots_on_goalrow)
+
+pointsrow = "points " + Math.floor(thisComp.layer("Clayton Keller.csv")("Effects")("Spreadsheet Row")("Slider"));
+thisComp.layer("Clayton Keller.csv")("ADBE Data Group")("Outline")("points")(pointsrow)
+
+values = [];for(i=0;i<=thisComp.layer("Scoreboard2.csv")("ADBE Data Group")("ADBE DataLayer Num Rows")-1;i++) {     values.push(thisComp.layer("Scoreboard2.csv")("ADBE Data Group")("Outline")("Stat Data 01")("Stat Data 01 " + i).value); } values.join("\n");
+
+
+
+
+a = thisComp.layer("CONTROLS").effect("Text Sliding Amplitude")("Slider");
+const {width, height} = sourceRectAtTime()
+amp = (width - 1280) / 2
+f = (thisComp.layer("CONTROLS").effect("Text Sliding Frequency")("Slider") / 100);
+x = Math.sin(time*2*Math.PI*f)*amp;
+y = value[1];
+
+[x,y]
+
+
+
+
+{
+    playernumber = (thisComp.layer("Player Number").text.sourceText).value
+
+i=0
+
+findrow = thisComp.layer("2020-2021 Arizona Coyotes Player Season Totals - Natural Stat Trick.csv")("Data")("Outline")("Number")("Number " + i)
+
+
+
+result = i
+text.sourceText = result
+}
+
+
+
+const {width, height} = sourceRectAtTime(0)
+
+singleWordHeight = height / Math.floor(thisComp.layer("CONTROLS").effect("Height")("Slider").valueAtTime(0))
+x = value[0];
+y = (time * 100) % singleWordHeight
+
+[x,y]
+
+const {width, height} = sourceRectAtTime(0)
+
+singleWordWidth = width / Math.floor(thisComp.layer("CONTROLS").effect("Width")("Slider").valueAtTime(0))
+x = (time * 100) % singleWordWidth
+y = value[1];
+
+[x,y]
---
+++ Misc Expressions.js
@@ -0,0 +1,59 @@
+// Warp Settings
+thisComp.layer("CONTROLS").effect("Warp Amount")("Slider")
+thisComp.layer("CONTROLS").effect("Warp Size")("Slider")
+time * Math.floor(thisComp.layer("CONTROLS").effect("Warp SPEED")("Slider").valueAtTime(0))
+
+// Source Text
+x = thisComp.layer("CONTROLS").effect("Width")("Slider").valueAtTime(0)
+y = thisComp.layer("CONTROLS").effect("Height")("Slider").valueAtTime(0)
+if(thisComp.layer("CONTROLS").effect("Space Text")("Checkbox") == 0)
+	textField = thisComp.layer("COYOTES").text.sourceText;
+else textField = thisComp.layer("COYOTES").text.sourceText + " "
+
+textReady = textField.repeat(x) + "\n"
+wideText = textReady.repeat(y)
+
+wideText
+
+// Animators
+
+    // Sets how frequently the RNG runs
+posterizeTime(Math.floor(thisComp.layer("CONTROLS").effect("Random Highlight Frequency")("Slider").valueAtTime(0))*2);
+    // Imports number of words horizontally and vertically
+height = Math.floor(thisComp.layer("CONTROLS").effect("Height")("Slider").valueAtTime(0))
+width = Math.floor(thisComp.layer("CONTROLS").effect("Width")("Slider").valueAtTime(0))
+
+numberOfWords = height * width
+
+wordHighlighter = Math.floor(random(numberOfWords))
+
+if(textIndex === wordHighlighter) {
+ 100;
+} else {
+ 0;
+}
+
+a = thisComp.layer("CONTROLS").effect("Text Sliding Amplitude")("Slider");
+const {width, height} = sourceRectAtTime()
+amp = (width - 1280) / 2
+f = (thisComp.layer("CONTROLS").effect("Text Sliding Frequency")("Slider") / 100);
+x = Math.sin(time*2*Math.PI*f)*amp;
+y = value[1];
+
+[x,y]
+
+
+// Anchor Points
+const {width, height, left, top} = sourceRectAtTime(0);
+[left + width / 2, top + height / 2]
+
+
+// Position
+const {width, height} = sourceRectAtTime(0)
+
+singleWordWidth = width / Math.floor(thisComp.layer("CONTROLS").effect("Width")("Slider").valueAtTime(0))
+x = (time * 100) % singleWordWidth
+y = value[1];
+
+[x,y]
+
