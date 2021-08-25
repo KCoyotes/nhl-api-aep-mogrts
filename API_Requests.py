d84146084d02f253633a8783e0487350a1d14328
KCoyotes <78571203+KCoyotes@users.noreply.github.com>
2021-08-24 22:28:47 -0700

    Python scripting done, ready to be plugged into AE

--- API_Requests.py
+++ API_Requests.py
@@ -9,6 +9,18 @@ t = requests.get(teamURL)
 
 rosterData = r.json()
 teamData = t.json()
+finalData = {"Example Key" : "Example Value"}
+finalData.update(teamData)
+
+from datetime import datetime
+
+now = datetime.now()
+
+timestamp = now.strftime("%B %d, %Y %I:%M:%S %p")
+timestampStr = "Data generated on " + timestamp
+print(timestampStr)
+
+finalData['DEBUG'] = timestampStr
 
 with open('APIroster.json', 'w', encoding='utf-8') as f:
     json.dump(rosterData, f, ensure_ascii=False, indent=4)
@@ -24,12 +36,15 @@ for roster in team:
 for player in uniqueIDs:
     statsURL = 'https://statsapi.web.nhl.com/api/v1/people/'
     uniqueURL = statsURL + str(player) + '?hydrate=stats(splits=statsSingleSeason)'
-    print(uniqueURL)
     response = requests.get(uniqueURL)
-    print(response)
     playerStats = response.json()
-    print(player)
-    teamData['teams'].append(playerStats['people'])
+    print("Downloading stats for Player ID: " + str(player))
+    print(response)
+    finalData['teams'].append(playerStats['people'][0])
+
+print("\nDownload complete. Writing to JSON file...")
 
 with open('API_Stats.json', 'w', encoding='utf-8') as f:
-    json.dump(teamData, f, ensure_ascii=False, indent=4)
+    json.dump(finalData, f, ensure_ascii=False, indent=4)
+
+print("\nLETS GOOOOOOOOOOO\nAPI_Stats.json is ready for import into After Effects!\nStats are up-to-date from NHL.com as of " + timestamp + "\n")
