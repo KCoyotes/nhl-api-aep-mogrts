b727724ef3c124f1b09e328e36a81373bd39725b
Kevin <Kevin.Thompson@ArizonaCoyotes.com>
2021-08-24 20:49:57 -0700

    Python Scripting

--- API_Requests.py
+++ API_Requests.py
@@ -1,3 +1,32 @@
 import json
 import requests
 
+url = 'https://statsapi.web.nhl.com/api/v1/teams/53/?expand=team.roster'
+
+r = requests.get(url)
+
+data = r.json()
+
+with open('APIroster.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
+
+uniqueIDs = []
+
+team = data['teams'][0]['roster']['roster']
+
+for roster in team:
+    ID = roster['person']['id']
+    uniqueIDs.append(ID)
+
+for player in uniqueIDs:
+    statsURL = 'https://statsapi.web.nhl.com/api/v1/people/'
+    uniqueURL = statsURL + str(player) + '?hydrate=stats(splits=statsSingleSeason)'
+    print(uniqueURL)
+    response = requests.get(uniqueURL)
+    print(response)
+    playerStats = response.json()
+    print(player)
+    data['teams'][0]['roster']['roster'][0]['person'].append(playerStats['people'])
+
+with open('API_Stats.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
